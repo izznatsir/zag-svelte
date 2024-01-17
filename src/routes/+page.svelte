ca02404750760af526b408e919e59a4d3aaf8fb4
Izzuddin Natsir <32354148+izznatsir@users.noreply.github.com>
2024-01-17 16:54:02 +0700

    feat: implement hooks

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,30 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import * as accordion from '@zag-js/accordion';
+	import { normalizeProps, useMachine } from '$lib/dist/index.js';
+	import { unstate } from 'svelte';
+
+	const data = [
+		{ title: 'Watercraft', content: 'Sample accordion content' },
+		{ title: 'Automobiles', content: 'Sample accordion content' },
+		{ title: 'Aircrafts', content: 'Sample accordion content' }
+	];
+
+	const machine = useMachine(accordion.machine({ id: '1' }));
+
+	const api = $derived(accordion.connect(unstate(machine.state), machine.send, normalizeProps));
+</script>
+
+<div {...api.rootProps}>
+	{#each data as item}
+		<div {...api.getItemProps({ value: item.title })}>
+			<h3>
+				<button {...api.getItemTriggerProps({ value: item.title })}>
+					{item.title}
+				</button>
+			</h3>
+			<div {...api.getItemContentProps({ value: item.title })}>
+				{item.content}
+			</div>
+		</div>
+	{/each}
+</div>
